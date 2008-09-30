a23061b0a539adc0d8269cdc0a203274e6ae7335
Bernd Steinhauser <berniyh@exherbo.org>
2008-10-01 00:53:05 +0200

    Move lm_sensors to arbor.

---
+++ packages/sys-apps/lm_sensors/files/fancontrol-init.d
@@ -0,0 +1,35 @@
+#!/sbin/runscript
+# Copyright 2008 Bernd Steinhauser <berniyh@exherbo.org>
+# Distributed under the terms of the GNU General Public License v2
+# Based in part upon 'fancontrol-init.d' from Gentoo, which is:
+# Copyright 1999-2006 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+CONFIG=/etc/conf.d/fancontrol
+PID=/var/run/fancontrol.pid
+
+depend() {
+	after lm_sensors
+}
+
+checkconfig() {
+	if [ ! -f ${CONFIG} ]; then
+		eerror "Configuration file ${CONFIG} not found"
+		return 1
+	fi
+}
+
+start() {
+	checkconfig || return 1
+
+	ebegin "Starting fancontrol"
+	start-stop-daemon --start --quiet --background --pidfile ${PID} \
+		--exec /usr/sbin/fancontrol -- ${CONFIG}
+	eend ${?}
+}
+
+stop() {
+	ebegin "Stopping fancontrol"
+	start-stop-daemon --stop --pidfile ${PID}
+	eend ${?}
+}
---
+++ packages/sys-apps/lm_sensors/files/sensord-init.d
@@ -0,0 +1,35 @@
+#!/sbin/runscript
+# Copyright 2008 Bernd Steinhauser <berniyh@exherbo.org>
+# Distributed under the terms of the GNU General Public License v2
+# Based in part upon 'sensord-init.d' from Gentoo, which is:
+# Copyright 1999-2006 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+CONFIG=/etc/conf.d/sensord
+
+depend() {
+	need logger
+	use lm_sensors
+}
+
+checkconfig() {
+	if [ ! -f ${CONFIG} ]; then
+		eerror "Configuration file ${CONFIG} not found"
+		return 1
+	fi
+}
+
+start() {
+	checkconfig || return 1
+
+	ebegin "Starting sensord"
+	start-stop-daemon --start --exec /usr/sbin/sensord \
+		-- --config-file ${CONFIG} ${SENSORD_OPTIONS}
+	eend ${?}
+}
+
+stop() {
+	ebegin "Stopping sensord"
+	start-stop-daemon --stop --pidfile /var/run/sensord.pid
+	eend ${?}
+}
