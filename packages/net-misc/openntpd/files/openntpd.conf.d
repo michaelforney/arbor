67e8be519cd9e09e414a06d169c2131ff0765027
Daniel Mierswa <impulze@impulze.org>
2009-01-29 04:54:08 +0100

    trivial conf.d, use hereconfd

--- packages/net-misc/openntpd/files/openntpd.conf.d
+++
@@ -1,7 +0,0 @@
-# /etc/conf.d/ntpd: config file for openntpd's ntpd
-
-NTPD_HOME=/var/empty
-
-# See ntpd(8) man page ... some popular options:
-#  -s   Set the time immediately at startup
-NTPD_OPTS=""
