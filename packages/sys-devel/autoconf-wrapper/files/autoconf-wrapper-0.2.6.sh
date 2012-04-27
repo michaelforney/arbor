bebc6e7b35bb5031948f6eaf8dfe1f73448386fa
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2012-04-27 17:59:23 +0200

    bump autoconf-wrapper for autoconf 2.69
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- packages/sys-devel/autoconf-wrapper/files/autoconf-wrapper-0.2.6.sh
+++ packages/sys-devel/autoconf-wrapper/files/autoconf-wrapper-0.2.6.sh
@@ -4,10 +4,10 @@
 # Copyright 2007 Bryan Østergaard <kloeri@exherbo.org>
 # Copyright 2008 Bo Ørsted Andresen <zlin@exherbo.org>
 # Distributed under the terms of the GNU General Public License v2
-# Autoconf wrapper v0.2.3 -- http://www.exherbo.org/
+# Autoconf wrapper v0.2.6 -- http://www.exherbo.org/
 
 # Keep versions:slots sorted highest to lowest
-AUTOCONF_VERSIONS="2.68:2.5 2.13:2.1"
+AUTOCONF_VERSIONS="2.69:2.5 2.13:2.1"
 AUTOCONF_PROGRAM="$(basename $0)"
 
 # Default to latest available if WANT_AUTOCONF isn't set
