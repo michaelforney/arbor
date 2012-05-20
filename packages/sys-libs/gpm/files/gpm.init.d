a5ca0faaf39e7c77e1b4db7ffa2fbe5f506f20e5
Wulf C. Krueger <philantrop@exherbo.org>
2012-05-20 19:45:50 +0200

    gpm: Don't install to/use /var/run.

--- packages/sys-libs/gpm/files/gpm.init.d
+++ packages/sys-libs/gpm/files/gpm.init.d
@@ -6,33 +6,33 @@
 #NB: Config is in /etc/conf.d/gpm
 
 depend() {
-	need localmount
-	use hotplug logger
+    need localmount
+    use hotplug logger
 }
 
 checkconfig() {
-	if [ -z "$MOUSEDEV" ] || [ -z "$MOUSE" ] ; then
-		eerror "You need to setup MOUSEDEV and MOUSE in /etc/conf.d/gpm first"
-		return 1
-	fi
+    if [ -z "$MOUSEDEV" ] || [ -z "$MOUSE" ] ; then
+        eerror "You need to setup MOUSEDEV and MOUSE in /etc/conf.d/gpm first"
+        return 1
+    fi
 }
 
 start() {
-	checkconfig || return 1
+    checkconfig || return 1
 
-	local params=""
-	[ -n "$RESPONSIVENESS" ] && params="$params -r $RESPONSIVENESS"
-	[ -n "$REPEAT_TYPE" ]    && params="$params -R$REPEAT_TYPE"
-	[ -n "$APPEND" ]         && params="$params $APPEND "
-	
-	ebegin "Starting gpm"
-	start-stop-daemon --start --quiet --exec /usr/sbin/gpm \
-		-- -m ${MOUSEDEV} -t ${MOUSE} ${params}
-	eend ${?}
+    local params=""
+    [ -n "$RESPONSIVENESS" ] && params="$params -r $RESPONSIVENESS"
+    [ -n "$REPEAT_TYPE" ]    && params="$params -R$REPEAT_TYPE"
+    [ -n "$APPEND" ]         && params="$params $APPEND "
+
+    ebegin "Starting gpm"
+    start-stop-daemon --start --quiet --exec /usr/sbin/gpm \
+        -- -m ${MOUSEDEV} -t ${MOUSE} ${params}
+    eend ${?}
 }
 
 stop() {
-	ebegin "Stopping gpm"
-	start-stop-daemon --stop --quiet --pidfile /var/run/gpm.pid
-	eend ${?}
+    ebegin "Stopping gpm"
+    start-stop-daemon --stop --quiet --pidfile /run/gpm.pid
+    eend ${?}
 }
